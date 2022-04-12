484940f8004f679369034e953a11125244435d45
RayBar72 <3963@holbertonschool.com>
2022-04-12 10:24:34 -0300

    Update state

--- models/state.py
+++ models/state.py
@@ -4,20 +4,19 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('Cities', backref='state')
-
-    @property
-    def cities(self):
-        """List of City instances with id of state"""
-        from models import storage
-        new = []
-        for key, obje in storage.all(City).items():
-            if obje.state_id == self.id:
-                new.append(obje)
-        return new
+    cities = relationship('City', backref='state')
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """List of City instances with id of state"""
+            from models import storage
+            from models.city import City
+            return [v for k, v, in storage.all(City).items() if v.state_id == self.id]
