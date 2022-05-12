cb5dbbde9b1ff665d434b0533c627d9a5ddcdce3
andresovichh <ahenderson.holberton@gmail.com>
2022-05-12 13:10:27 -0300

    1 seems to work

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a Flask route"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world2():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
