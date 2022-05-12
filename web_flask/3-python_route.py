03bf94d6d51d51cc2f49f1debb38f7c90dbd6d50
andresovichh <ahenderson.holberton@gmail.com>
2022-05-12 14:11:35 -0300

    3 seems to work

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This is a module that contains a Flask route"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ prints 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world2():
+    """ prints 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    """ returns text with 'C' """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_text_python(text="is cool"):
+    """ returns text with 'C' """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
