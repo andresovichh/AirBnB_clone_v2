41c7acd7b56a298ad02e56594651b5b979920f68
andresovichh <ahenderson.holberton@gmail.com>
2022-05-12 13:05:03 -0300

    test 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,29 @@
-#!/usr/bin/env bash
-# Script that install nginx
-sudo apt-get update
-sudo apt-get -y install nginx
-echo "Hello World" > /var/www/html/index.nginx-debian.html
-sudo service nginx start
-sudo sed -i '/listen \[::\]:80 default_server;/a\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default
-sudo sed -i '/listen \[::\]:80 default_server;/a\\tadd_header X-Served-By \"\$HOSTNAME\";' /etc/nginx/sites-available/default
-sudo sed -i '/TGUlwu4 permanent;$/a\\terror_page 404 /custom_404.html;' /etc/nginx/sites-available/default
-echo "Ceci n'est pas une page" >/var/www/html/custom_404.html
-sudo service nginx restart
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello from RayBar!" > /data/web_static/releases/test/index.html
+"/data/web_static/releases/test/"
+ )
+# create directories
+ for i in "${dirs[@]}"
+ do
+ if [[ ! -f "$i" ]]
+ then
+ sudo mkdir -p "$i"
+ fi
+ done
+
+# create html
+if [[ ! -f "/data/web_static/releases/test/index.html" ]]
+then
+sudo touch "/data/web_static/releases/test/index.html"
+echo "Hola Andres" > /data/web_static/releases/test/index.html
+fi
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+"0-setup_web_static.sh" 40L, 1076C                       33,21         75%
